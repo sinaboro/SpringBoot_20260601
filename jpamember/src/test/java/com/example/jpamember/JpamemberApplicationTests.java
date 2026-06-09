a0899fd9cc3416acba657017891eb90a1b5b763f
kimdaechul <rlaeocjf@gmail.com>
2026-06-09 09:48:03 +0900

    commit

---
+++ jpamember/src/test/java/com/example/jpamember/JpamemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpamember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpamemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
