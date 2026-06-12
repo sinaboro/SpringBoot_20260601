d3343723567b38f54e426b52f5d0024e2499cd57
kimdaechul <rlaeocjf@gmail.com>
2026-06-12 09:56:25 +0900

    commit

---
+++ jpasecurity/src/main/java/com/example/jpasecurity/JpasecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpasecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpasecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpasecurityApplication.class, args);
+    }
+
+}
